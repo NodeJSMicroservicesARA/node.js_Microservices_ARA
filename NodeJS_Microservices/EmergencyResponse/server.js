de28f791aecc4125e4e21eada3fd2443cfd2f147
maisaa <30745871+maisaa@users.noreply.github.com>
2023-06-05 23:53:08 +0200

    emergency response

--- NodeJS_Microservices/EmergencyResponse/server.js
+++ NodeJS_Microservices/EmergencyResponse/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const connectDB = require('./db');
+const emergencyRoutes = require('./routes');
+
+
 const app = express();
 const port = 8082;
 
+// Connect to MongoDB
+connectDB();
+
+// Set up middleware
+app.use(express.json());
+
+// Set up routes
+app.use('/emergencies', emergencyRoutes);
 
 app.listen(port, ()=> {
     console.log('Emergency Response Service running on port 8082');
